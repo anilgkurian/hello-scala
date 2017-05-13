464957e246c72224c00362670a216fdea5a46db0
Anil Kurian <anilgkurian@live.com>
2017-05-13 12:38:10 +0530

    updated loan pattern

--- src/main/scala-2.11/org/anil/scala/patterns/LoanPattern.scala
+++ src/main/scala-2.11/org/anil/scala/patterns/LoanPattern.scala
@@ -15,4 +15,25 @@ class LoanPattern {
       writer.close()
   }
 
+  def withCurryingPrintWriter(file: File)(op: PrintWriter => Unit): Unit = {
+    val writer = new PrintWriter(file)
+    try
+      op(writer)
+    finally
+      writer.close()
+  }
+
+  def main(args: Array[String]): Unit = {
+    withPrintWriter(
+      new File(""),
+      writer => writer.println("hello")
+    )
+
+    // can use {} here as it takes only one param
+    val file = new File("")
+
+    withCurryingPrintWriter(file) {
+      writer => writer.println("hello")
+    }
+  }
 }
